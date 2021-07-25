1485e32bb54705d482764da015798ff1002ff4a9
Stanislav Prokhorov <stas.prokh@gmail.com>
2021-07-24 23:07:49 -0400

    Added a basic play

--- solution.js
+++ solution.js
@@ -18,5 +18,36 @@ function humanPlay() {
 }
 
 function play(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie! " + playerSelection + " is equal to " + computerSelection);
+    }
+    else {
+        if (playerSelection === "rock") {
+            if (computerSelection === "paper") {
+                console.log("You lose! " + playerSelection + " loses to " + computerSelection);
+            }
+            if (computerSelection === "scissors") {
+                console.log("You win! " + playerSelection + " beats " + computerSelection);
+            }
+        }
 
-}
+        else if (playerSelection === "paper") {
+            if (computerSelection === "rock") {
+                console.log("You win! " + playerSelection + " beats " + computerSelection);
+            }
+            if (computerSelection === "scissors") {
+                console.log("You lose! " + playerSelection + " loses to " + computerSelection);
+            }
+        }
+        
+        else if (playerSelection === "scissors") {
+            if (computerSelection === "rock") {
+                console.log("You lose! " + playerSelection + " loses to " + computerSelection);
+            }
+            if (computerSelection === "paper") {
+                console.log("You win! " + playerSelection + " loses to " + computerSelection);
+            }
+        }
+
+    }
+    }
